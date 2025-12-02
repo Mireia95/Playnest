0cbcb95539e3701ac123c2b7aec4ef0e50720e10
Mireia <mireia95.granzotto@gmail.com>
2025-12-02 20:47:06 +0100

    update GuessColor: create useReducer

--- src/components/GuessColor/PlayGame/PlayGame.jsx
+++ src/components/GuessColor/PlayGame/PlayGame.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import CardsColorDiv from '../CardsColorDiv/CardsColorDiv'
 import './PlayGame.css'
 import { useEffect } from 'react'
 import GameOver from '../GameOver/GameOver'
 import Timer from '../Timer/Timer'
 import ColorPrompt from '../ColorPrompt/ColorPrompt'
+import {
+  GUESSCOLOR_INITIAL_STATE,
+  guessColorReducer
+} from '../../../reducer/GuessColor/reducer'
 
 const PlayGame = () => {
+  const [state, dispatch] = useReducer(
+    guessColorReducer,
+    GUESSCOLOR_INITIAL_STATE
+  )
+
+  const { points, colorPrint, colorOptions, colorSelected, gameover } = state
+  /* 
   useEffect(() => {
-    /*   const newArray = getRandomColors(colors);
-    setColorText(newArray.text); */
-  }, [])
+     const newArray = getRandomColors(colors);
+    setColorText(newArray.text); 
+  }, []) */
 
   return (
     <>
---
+++ src/reducer/GuessColor/reducer.js
@@ -0,0 +1,22 @@
+export const GUESSCOLOR_INITIAL_STATE = {
+  points: 0,
+  colorPrint: '',
+  colorOptions: [],
+  colorSelected: '',
+  gameover: false
+}
+
+export const guessColorReducer = (state, action) => {
+  switch (action.type) {
+    case 'SUM_POINT':
+      return { ...state, points: state.points + 5 }
+    case 'SET_COLOR_OPTIONS':
+      return { ...state, colorOptions: [...action.payload] }
+    case 'SET_COLOR_PRINT':
+      return { ...state, colorPrint: { ...action.payload } }
+    case 'SELECT_COLOR':
+      return { ...state, colorSelected: { ...action.payload } }
+    default:
+      return state
+  }
+}
