353d3beffe6de6d66145b12f4bf24f0283b1e02e
Mireia <mireia95.granzotto@gmail.com>
2026-01-13 07:54:41 +0100

    update comments shuffle

--- src/reducer/MemoryGame/actions.js
+++ src/reducer/MemoryGame/actions.js
@@ -6,8 +6,9 @@ export const shuffleCards = (dispatch) => {
   //array para shuffle
   let cardsForGame = [...duplicateCards];
 
+  //shuffle usando tecnica Fisher Yates
   for (let i = cardsForGame.length - 1; i > 0; i--) {
-    const randomPos = Math.floor(Math.random() * (i + 1));
+    const randomPos = Math.floor(Math.random() * (i + 1)); //genero un numero enntre 0 y i
 
     const savedPos = cardsForGame[i];
     cardsForGame[i] = cardsForGame[randomPos];
